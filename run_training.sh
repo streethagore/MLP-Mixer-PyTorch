47ce6c192be90cfeee5ec590ab4fd58a5519b990
streethagore <srivaud972@gmail.com>
2025-01-22 16:05:48 +0100

    Implement training script.

---
+++ run_training.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=mlpmixer
+#SBATCH --output=slurm/%x-%j.out
+#SBATCH --error=slurm/%x-%j.err
+#SBATCH --gres=gpu:1
+#SBATCH --time=24:00:00
+
+# Source the shell configuration file to apply changes (need for conda)
+source /home/tau/strivaud/.bashrc
+
+# activate the conda environment
+conda activate gromo
+
+# Run the training script
+command="python train.py"
+command+=" --batch_size 128"
+command+=" --epochs 300"
+command+=" --lr 1e-3"
+command+=" --weight_decay 5e-5"
+command+=" --num_workers 4"
+command+=" --device cuda"
+command+=" --augmentation autoaugment"
+
+eval "$command"
